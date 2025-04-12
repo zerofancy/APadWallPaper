77aaca233ce8333e36a87662d188c2b829eb82ed
zerofancy <ntutn.top@gmail.com>
2025-04-13 03:09:58 +0800

    feat(MainActivity): 添加壁纸设置按钮功能

--- app/src/main/java/top/ntutn/apadwallparper/MainActivity.kt
+++ app/src/main/java/top/ntutn/apadwallparper/MainActivity.kt
@@ -1,11 +1,15 @@
 package top.ntutn.apadwallparper
 
+import android.app.WallpaperManager
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,28 +24,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             APadWallparperTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Box(
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        Button(onClick = {
+                            val intent = Intent()
+                            intent.action = WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER
+                            startActivityForResult(intent, 0)
+                        }) {
+                            Text("设为壁纸")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    APadWallparperTheme {
-        Greeting("Android")
     }
 }

b423d068d84cffb887a0217484f3dbbc1d863ba4
zerofancy <ntutn.top@gmail.com>
2025-04-14 01:45:29 +0800

    feat: 添加MMKV支持并重构主界面布局
    scope: MainActivity,MyApplication,gradle配置
    
    - 新增MyApplication初始化MMKV
    - 主界面布局由Box改为Column并添加新按钮
    - 更新gradle配置添加MMKV依赖
    - 修改AndroidManifest注册Application

--- app/src/main/java/top/ntutn/apadwallparper/MainActivity.kt
+++ app/src/main/java/top/ntutn/apadwallparper/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -29,12 +29,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             APadWallparperTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(
+                    Column (
                         modifier = Modifier.padding(innerPadding)
                     ) {
                         Button(onClick = {
+
+                        }) {
+                            Text("选择横屏壁纸")
+                        }
+                        Button(onClick = {
+
+                        }) {
+                            Text("选择竖屏壁纸")
+                        }
+                        Button(onClick = {
                             val intent = Intent()
                             intent.action = WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                             launcher.launch(intent)
                         }) {
                             Text("设为壁纸")
